c691806f34ca8d3e9c80388a6e9b856d43c5af8a
Bina <benjaminbinyam500@gmail.com>
2025-02-04 07:08:54 +0300

    finished the starter useState

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState());
+  // console.log(useState("Current value"));
+  // console.log(useState(0));
+  // const value = useState("Hello")[0];
+  // const func = useState()[1];
+  // console.log(value);
+  // console.log(func);
+
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    // console.log(count);
+    setCount(count + 1);
+    // be careful, we can set any value
+    // setCount('pants');
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button className="btn" onClick={handleClick}>
+        increment
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
