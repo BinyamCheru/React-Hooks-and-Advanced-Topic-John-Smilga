b85d5c763f456bdaa7a59cb3691807f04a493bc5
Bina <benjaminbinyam500@gmail.com>
2025-02-04 20:27:31 +0300

    github user challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
+import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
+import Final from "./tutorial/02-useEffect/final/04-fetch-data";
 
 function App() {
   return (
     <div className="container">
       <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
