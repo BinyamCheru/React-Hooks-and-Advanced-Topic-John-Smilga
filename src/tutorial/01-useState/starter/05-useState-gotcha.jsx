9655071740111b24319b47ee236d9ec6af53390b
Bina <benjaminbinyam500@gmail.com>
2025-02-04 11:31:51 +0300

    functional update approach example

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
   const handleClick = () => {
-    setValue((currentValue) => {
-      const newValue = currentValue + 1;
-      console.log(newValue);
-      return newValue;
-    });
+    setTimeout(() => {
+      console.log("Button clicked");
+      setValue((currentValue) => {
+        return currentValue + 1;
+      });
+    }, 3000);
   };
   return (
     <div>
