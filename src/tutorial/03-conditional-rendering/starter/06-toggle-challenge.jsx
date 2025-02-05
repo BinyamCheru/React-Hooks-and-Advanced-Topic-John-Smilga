79a3ee5c9acfc18588f2a870527cb27b94a52d16
Bina <benjaminbinyam500@gmail.com>
2025-02-05 06:46:10 +0300

    toggle challenge i tried

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isTrue, setIsTrue] = useState(true);
+
+  const handleClick = () => {
+    isTrue ? setIsTrue(false) : setIsTrue(true);
+  };
+  return (
+    <div>
+      <button className="btn" onClick={handleClick}>
+        Toggle
+      </button>
+      {isTrue ? <SomeComponent /> : <h2>Hello</h2>}
+    </div>
+  );
 };
 
+const SomeComponent = () => {
+  return (
+    <div>
+      <h2>Render the component</h2>
+    </div>
+  );
+};
 export default ToggleChallenge;
