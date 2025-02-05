acf8a63b6a7d105061db96dea5842b10895aeeb0
Bina <benjaminbinyam500@gmail.com>
2025-02-05 09:10:30 +0300

    user challenge

--- src/tutorial/03-conditional-rendering/final/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/final/07-user-challenge.jsx
@@ -1,27 +1,31 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const UserChallenge = () => {
   const [user, setUser] = useState(null);
+
   const login = () => {
     // normally connect to db or api
-    setUser({ name: "vegan food truck" });
+    setUser({ name: 'vegan food truck' });
   };
   const logout = () => {
     setUser(null);
   };
+
   return (
     <div>
       {user ? (
         <div>
-          <h4>hello, there {user.name}</h4>
-          <button className="btn" onClick={logout}>
+          <h4>hello there, {user.name}</h4>
+          <button className='btn' onClick={logout}>
             logout
           </button>
         </div>
       ) : (
         <div>
-          <h4>Please login</h4>
-          <button className="btn" onClick={login}>Login</button>
+          <h4>Please Login</h4>
+          <button className='btn' onClick={login}>
+            login
+          </button>
         </div>
       )}
     </div>
