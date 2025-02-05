8dd3dc808e8e2769be2ba7a3c9af64e6b416aad2
Bina <benjaminbinyam500@gmail.com>
2025-02-05 06:31:28 +0300

    ternary operator

--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -6,7 +6,7 @@ const ShortCircuitExamples = () => {
   // truthy
   const [name, setName] = useState("susan");
   const [user, setUser] = useState({ name: "john" });
-  const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(true);
 
   return (
     <div>
@@ -24,17 +24,29 @@ const ShortCircuitExamples = () => {
           <h2>{name}</h2>
         </div>
       )}
-      {user && <SomeComponent name={user.name}/>}
+      {user && <SomeComponent name={user.name} />}
+      <h2 style={{ margin: "1rem 0" }}>Ternary Operator</h2>
+      <button className="btn">{isEditing ? "edit" : "add"}</button>
+      {user ? (
+        <div>
+          <h4>Hello there user {user.name} </h4>
+        </div>
+      ) : (
+        <div>
+          <h2>please login</h2>
+        </div>
+      )}
     </div>
   );
 };
 
-const SomeComponent = ({name})=>{
+const SomeComponent = ({ name }) => {
   return (
     <div>
       <h2> whatever return</h2>
       <h2>{name}</h2>
+      <button className="btn">Log out</button>
     </div>
   );
-}
+};
 export default ShortCircuitExamples;
