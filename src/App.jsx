f5f64bbfe60f6e3f5df36e90a207120d76aa5484
Bina <benjaminbinyam500@gmail.com>
2025-02-05 05:40:28 +0300

    conditional-rendering setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx";
-import Final from "./tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data.jsx";
+import Starter from "./tutorial/03-conditional-rendering/starter/03-hooks-rule.jsx";
+
 
 function App() {
   return (
