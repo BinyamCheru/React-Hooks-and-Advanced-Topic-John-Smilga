43a5d017c858f4af41ea428ffb9f056e425cce24
Bina <benjaminbinyam500@gmail.com>
2025-02-10 09:30:04 +0300

    formData API

--- src/tutorial/06-forms/starter/05-form-data.jsx
+++ src/tutorial/06-forms/starter/05-form-data.jsx
@@ -1,43 +1,55 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UncontrolledInputs = () => {
   const [value, setValue] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // console.log(e.currentTarget);
+    const formData = new FormData(e.currentTarget);
+    console.log(formData);
+    // const name = formData.get("name");  // get the values using the name attribute of the inputs
+    // const email = formData.get("email");
+    // console.log(name);
+    // console.log(email);
+    // console.log([...formData.entries()]); // form data is an array of arrays
+    const newUser = Object.fromEntries(formData);
+    console.log(newUser);
+    setValue(value + 1); // event if we render the values are still going to be there
+    e.currentTarget.reset();
   };
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Form Data API</h4>
         {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' name='name' />
+          <input type="text" className="form-input" id="name" name="name" />
         </div>
         {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type='email' className='form-input' id='email' name='email' />
+          <input type="email" className="form-input" id="email" name="email" />
         </div>
         {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
           <input
-            type='password'
-            className='form-input'
-            id='password'
-            name='password'
+            type="password"
+            className="form-input"
+            id="password"
+            name="password"
           />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
