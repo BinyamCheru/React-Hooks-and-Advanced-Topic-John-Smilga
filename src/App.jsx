80d9254d40ee0807e31087eeb54aae2b3bffa090
Bina <benjaminbinyam500@gmail.com>
2025-02-11 10:06:57 +0300

    using custom hook to reduce the import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from "./tutorial/09-context-api/final/prop-drilling/Navbar";
-import Starter from "./tutorial/09-context-api/starter/Navbar";
+import Starter from "./tutorial/09-context-api/starter/context API/Navbar";
 
 function App() {
   return (
--- src/tutorial/09-context-api/starter/context API/Navbar.jsx
+++ src/tutorial/09-context-api/starter/context API/Navbar.jsx
@@ -1,8 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import NavLinks from "./NavLinks";
 
 export const NavbarContext = createContext();
 // console.log(NavbarContext);
+
+// custom hook
+export const useAppContext = () => useContext(NavbarContext);
 
 const Navbar = () => {
   const [user, setUser] = useState({ name: "bob" });
